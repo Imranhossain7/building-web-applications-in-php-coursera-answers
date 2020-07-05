7eb8c77d53b15558d392fceeca140664d123a209
Imran Hossain <imranice.nstu.12@gmail.com>
2020-07-05 13:56:09 +0600

    Wekk 5 added

---
+++ week 5/assignment/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Imran hossain PHP</title>
+	</head>
+	<body>
+		<h1>Imran Hosssain PHP</h1>
+
+		<pre>ASCII ART (First letter of the Name 'Imran' - 'I'):
+
+    ****
+     **
+     **
+     **
+     **
+     **
+    ****
+		</pre>
+
+		<p>
+			The SHA256 hash of "Imran Hossain" is <?php print hash('sha256', 'Imran Hossain'); ?>
+		</p>
+	
+	</body>
+</html>
